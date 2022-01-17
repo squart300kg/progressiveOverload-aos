eb828fff9472107b568df05dcd73a94eb1d23fa5
sangyunsong <squart300kg@naver.com>
2022-01-17 11:59:35 +0900

    1rm입력 창 ui꾸밈

--- app/src/main/java/com/progressive/overload/ui/dialog/Input1RMDialog.kt
+++ app/src/main/java/com/progressive/overload/ui/dialog/Input1RMDialog.kt
@@ -4,25 +4,32 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.Window
 import com.progressive.overload.R
 import com.progressive.overload.base.BaseCenterDialog
 import com.progressive.overload.databinding.FragmentInput1rmDialogBinding
 import dagger.hilt.android.AndroidEntryPoint
+import android.content.DialogInterface
+import android.view.*
+
 
 @AndroidEntryPoint
 class Input1RMDialog(
     private val onClickCancel: () -> Unit,
-    private val onClickOk: (String, String, String, String) -> Unit
+    private val onClickOk: (String, String, String, String) -> Unit,
 ) : BaseCenterDialog<FragmentInput1rmDialogBinding>(R.layout.fragment_input_1rm_dialog) {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = super.onCreateDialog(savedInstanceState)
         dialog.setCancelable(false)
         dialog.setCanceledOnTouchOutside(false)
+        dialog.setOnKeyListener { _, keyCode, keyEvent ->
+            // getAction to make sure this doesn't double fire
+            if (keyCode == KeyEvent.KEYCODE_BACK && keyEvent.action === KeyEvent.ACTION_UP) {
+                onClickCancel()
+                true // Capture onKey
+            } else false
+            // Don't capture
+        }
         return dialog
     }
 
